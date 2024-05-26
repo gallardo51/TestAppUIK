147f38291770f3fc2ffe3920b75c73df08f564c4
Alex Sobolev <31108@mail.ru>
2024-05-26 15:37:22 +0300

    1. Start app 2. Setup AppDelegate, info.plist and delete SceneDelegate, Storyboard

---
+++ TestAppUIK/LogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogInViewController.swift
+//  TestAppUIK
+//
+//  Created by Александр Соболев on 26.05.2024.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
