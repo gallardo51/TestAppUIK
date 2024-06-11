31beda8608a23338c4f27ed6f232af60e2373e26
Alex Sobolev <31108@mail.ru>
2024-06-11 16:28:58 +0300

    1. Setup QuestionsVC 2. Create ResultsVC

--- TestAppUIK/QuestionsViewController.swift
+++ TestAppUIK/QuestionsViewController.swift
@@ -18,6 +18,7 @@ class QuestionsViewController: UIViewController {
     
     private lazy var progressView: UIProgressView = {
         let progressView = UIProgressView()
+        progressView.progressTintColor = .systemGreen
         progressView.translatesAutoresizingMaskIntoConstraints = false
         return progressView
     }()
@@ -30,26 +31,194 @@ class QuestionsViewController: UIViewController {
         return questionLabel
     }()
     
-    private lazy var answerLabels: UILabel = {
+    private lazy var answerLabel1: UILabel = {
         let answerLabel = UILabel()
         answerLabel.numberOfLines = 0
         answerLabel.translatesAutoresizingMaskIntoConstraints = false
         return answerLabel
     }()
     
-    private lazy var switchAnswer: UISwitch = {
+    private lazy var answerLabel2: UILabel = {
+        let answerLabel = UILabel()
+        answerLabel.numberOfLines = 0
+        answerLabel.translatesAutoresizingMaskIntoConstraints = false
+        return answerLabel
+    }()
+    
+    private lazy var answerLabel3: UILabel = {
+        let answerLabel = UILabel()
+        answerLabel.numberOfLines = 0
+        answerLabel.translatesAutoresizingMaskIntoConstraints = false
+        return answerLabel
+    }()
+    
+    private lazy var answerLabel4: UILabel = {
+        let answerLabel = UILabel()
+        answerLabel.numberOfLines = 0
+        answerLabel.translatesAutoresizingMaskIntoConstraints = false
+        return answerLabel
+    }()
+    
+    private lazy var answerLabel5: UILabel = {
+        let answerLabel = UILabel()
+        answerLabel.numberOfLines = 0
+        answerLabel.translatesAutoresizingMaskIntoConstraints = false
+        return answerLabel
+    }()
+    
+    private lazy var answerLabel6: UILabel = {
+        let answerLabel = UILabel()
+        answerLabel.numberOfLines = 0
+        answerLabel.translatesAutoresizingMaskIntoConstraints = false
+        return answerLabel
+    }()
+    
+    private lazy var answerLabel7: UILabel = {
+        let answerLabel = UILabel()
+        answerLabel.numberOfLines = 0
+        answerLabel.translatesAutoresizingMaskIntoConstraints = false
+        return answerLabel
+    }()
+    
+    private lazy var answerLabel8: UILabel = {
+        let answerLabel = UILabel()
+        answerLabel.numberOfLines = 0
+        answerLabel.translatesAutoresizingMaskIntoConstraints = false
+        return answerLabel
+    }()
+    
+    private lazy var switchAnswer1: UISwitch = {
         let switchAnswer = UISwitch()
         switchAnswer.translatesAutoresizingMaskIntoConstraints = false
         return switchAnswer
     }()
     
-    private lazy var stackView: UIStackView = {
+    private lazy var switchAnswer2: UISwitch = {
+        let switchAnswer = UISwitch()
+        switchAnswer.translatesAutoresizingMaskIntoConstraints = false
+        return switchAnswer
+    }()
+    
+    private lazy var switchAnswer3: UISwitch = {
+        let switchAnswer = UISwitch()
+        switchAnswer.translatesAutoresizingMaskIntoConstraints = false
+        return switchAnswer
+    }()
+    
+    private lazy var switchAnswer4: UISwitch = {
+        let switchAnswer = UISwitch()
+        switchAnswer.translatesAutoresizingMaskIntoConstraints = false
+        return switchAnswer
+    }()
+    
+    private lazy var switchAnswer5: UISwitch = {
+        let switchAnswer = UISwitch()
+        switchAnswer.translatesAutoresizingMaskIntoConstraints = false
+        return switchAnswer
+    }()
+    
+    private lazy var switchAnswer6: UISwitch = {
+        let switchAnswer = UISwitch()
+        switchAnswer.translatesAutoresizingMaskIntoConstraints = false
+        return switchAnswer
+    }()
+    
+    private lazy var switchAnswer7: UISwitch = {
+        let switchAnswer = UISwitch()
+        switchAnswer.translatesAutoresizingMaskIntoConstraints = false
+        return switchAnswer
+    }()
+    
+    private lazy var switchAnswer8: UISwitch = {
+        let switchAnswer = UISwitch()
+        switchAnswer.translatesAutoresizingMaskIntoConstraints = false
+        return switchAnswer
+    }()
+    
+    private lazy var stackView1: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
-        stackView.distribution = .fillEqually
-        stackView.spacing = 140
-        stackView.addArrangedSubview(answerLabels)
-        stackView.addArrangedSubview(switchAnswer)
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 10
+        stackView.addArrangedSubview(answerLabel1)
+        stackView.addArrangedSubview(switchAnswer1)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var stackView2: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 10
+        stackView.addArrangedSubview(answerLabel2)
+        stackView.addArrangedSubview(switchAnswer2)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var stackView3: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 10
+        stackView.addArrangedSubview(answerLabel3)
+        stackView.addArrangedSubview(switchAnswer3)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var stackView4: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 10
+        stackView.addArrangedSubview(answerLabel4)
+        stackView.addArrangedSubview(switchAnswer4)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var stackView5: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 10
+        stackView.addArrangedSubview(answerLabel5)
+        stackView.addArrangedSubview(switchAnswer5)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var stackView6: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 10
+        stackView.addArrangedSubview(answerLabel6)
+        stackView.addArrangedSubview(switchAnswer6)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var stackView7: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 10
+        stackView.addArrangedSubview(answerLabel7)
+        stackView.addArrangedSubview(switchAnswer7)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var stackView8: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 10
+        stackView.addArrangedSubview(answerLabel8)
+        stackView.addArrangedSubview(switchAnswer8)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
@@ -58,7 +227,10 @@ class QuestionsViewController: UIViewController {
         let button = UIButton()
         button.setTitle("Следующий вопрос >>>", for: .normal)
         button.setTitleColor(.systemBlue, for: .normal)
-        button.addTarget(self, action: #selector(nextQuestion), for: .touchUpInside)
+        button.addTarget(
+            self,
+            action: #selector(answerButtonPressed),
+            for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -67,12 +239,12 @@ class QuestionsViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         setNavBar()
-        setupSubviews(progressView, questionLabel, stackView, nextQuestionButton)
+        setupSubviews(progressView, questionLabel, stackView1, stackView2, stackView3, stackView4, stackView5, stackView6, stackView7, stackView8, nextQuestionButton)
         setConstraints()
+        updateUI()
     }
     
     private func setNavBar() {
-        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
         navigationController?.navigationBar.prefersLargeTitles = true
         
         let navBarAppearance = UINavigationBarAppearance()
@@ -100,30 +272,24 @@ class QuestionsViewController: UIViewController {
         }
     }
     
-    private func addAnswers() {
-        let numberOfAnswers = currentAnswers.count
+    private func addAnswers(with answers: [Answer]) {
+        let answerArray = [stackView1, stackView2, stackView3, stackView4, stackView5, stackView6, stackView7, stackView8]
         
-        for answer in 1...numberOfAnswers {
-            let label = answerLabel
-            let switchAnswer = switchAnswer
-            label.text = answer.title
+        for _ in answerArray {
+            let label = [answerLabel1, answerLabel2, answerLabel3, answerLabel4, answerLabel5, answerLabel6, answerLabel7, answerLabel8]
             
-            for (switchAnswer, label) in zip(switchAnswer, currentAnswers) {
-                if switchAnswer.isOn {
-                    answersChosen.append(answer)
-                }
+            for (label, answer) in zip(label, answers) {
+                label.text = answer.title
             }
-            nextQuestion()
         }
-            
-        }
+    }
     
     private func setConstraints() {
         
         NSLayoutConstraint.activate([
             progressView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
-            progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-            progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0)
+            progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -10),
+            progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: +10)
         ])
         
         NSLayoutConstraint.activate([
@@ -133,49 +299,107 @@ class QuestionsViewController: UIViewController {
         ])
         
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: questionLabel.bottomAnchor, constant: 60),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+            stackView1.topAnchor.constraint(equalTo: questionLabel.bottomAnchor, constant: 60),
+            stackView1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView1.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
         ])
         
         NSLayoutConstraint.activate([
-            nextQuestionButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 40),
+            stackView2.topAnchor.constraint(equalTo: stackView1.bottomAnchor, constant: 10),
+            stackView2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView2.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView3.topAnchor.constraint(equalTo: stackView2.bottomAnchor, constant: 10),
+            stackView3.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView3.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView4.topAnchor.constraint(equalTo: stackView3.bottomAnchor, constant: 10),
+            stackView4.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView4.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView5.topAnchor.constraint(equalTo: stackView4.bottomAnchor, constant: 10),
+            stackView5.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView5.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView6.topAnchor.constraint(equalTo: stackView5.bottomAnchor, constant: 10),
+            stackView6.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView6.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView7.topAnchor.constraint(equalTo: stackView6.bottomAnchor, constant: 10),
+            stackView7.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView7.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView8.topAnchor.constraint(equalTo: stackView7.bottomAnchor, constant: 10),
+            stackView8.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView8.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            nextQuestionButton.topAnchor.constraint(equalTo: stackView8.bottomAnchor, constant: 40),
             nextQuestionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60),
             nextQuestionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -60)
         ])
         
     }
     
-    @objc private func nextQuestion() {
+    @objc private func answerButtonPressed() {
+        let switchAnswer = [switchAnswer1, switchAnswer2, switchAnswer3, switchAnswer4, switchAnswer5, switchAnswer6, switchAnswer7, switchAnswer8]
+        for (switchAnswer, answer) in zip(switchAnswer, currentAnswers) {
+            if switchAnswer.isOn {
+                answersChosen.append(answer)
+            }
+        }
+        nextQuestion()
+    }
+    
+    private func nextQuestion() {
         questionIndex += 1
+        
+        switchAnswer1.isOn = false
+        switchAnswer2.isOn = false
+        switchAnswer3.isOn = false
+        switchAnswer4.isOn = false
+        switchAnswer5.isOn = false
+        switchAnswer6.isOn = false
+        switchAnswer7.isOn = false
+        switchAnswer8.isOn = false
         
         if questionIndex < questions.count {
             updateUI()
             return
         }
+        
+        let resultsVC = ResultsViewController()
+        resultsVC.answers = answersChosen
+        resultsVC.modalPresentationStyle = .automatic
+        present(resultsVC, animated: true)
+        
     }
 }
 
 extension QuestionsViewController {
     private func updateUI() {
-        
-        // Get current question
+
         let currentQuestion = questions[questionIndex]
-        
-        // Set current question for question label
         questionLabel.text = currentQuestion.title
         
-        // Calculate progress
-        let totalProgress = Float(questionIndex) / Float(questions.count)
-        
-        // Set progress for question progress view
+        let totalProgress = Float(questionIndex) / Float(questions.count - 1)
         progressView.setProgress(totalProgress, animated: true)
         
-    }
-    
-    private func showCurrentAnswers(with answers: [Answer]) {
-        for (label, answer) in zip(answerLabel, answers) {
-            answerLabel.text = answer.title
-        }
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        addAnswers(with: currentAnswers)
     }
 }
---
+++ TestAppUIK/ResultsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ResultsViewController.swift
+//  TestAppUIK
+//
+//  Created by Александр Соболев on 11.06.2024.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    var answers: [Answer]!
+    
+    private lazy var questionLabel: UILabel = {
+        let questionLabel = UILabel()
+        questionLabel.font = .boldSystemFont(ofSize: 20)
+        questionLabel.numberOfLines = 0
+        questionLabel.translatesAutoresizingMaskIntoConstraints = false
+        return questionLabel
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupSubviews(questionLabel)
+        updateUI(with: answers)
+        
+    }
+    
+    private func setupSubviews(_ subviews: UIView...) {
+        subviews.forEach { subview in
+            view.addSubview(subview)
+        }
+    }
+    
+    private func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            questionLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            questionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            questionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+        
+    }
+    
+    private func updateUI(with answer: [Answer]) {
+        questionLabel.text = "Вопрос 1: \(answer[0].title),\nВопрос 2: \(answer[1].title), \nВопрос 3: \(answer[2].title), \nВопрос 4: \(answer[3].title), \nВопрос 5: \(answer[4].title), \nВопрос 6: \(answer[5].title)."
+        print(answers)
+    }
+}
+
